a75ca23a274c9f98d10924f0c2f42625da870983
Aires23Matos <aires2matos18@gmail.com>
2025-09-29 00:47:08 +0100

    new keys

--- mailtrap/mailtrap.config.js
+++ mailtrap/mailtrap.config.js
@@ -2,7 +2,7 @@ import { MailtrapClient } from "mailtrap";
 import dotenv from "dotenv";
 
 dotenv.config();
-const TOKEN = process.env.MAILTRAP_TOKEN; 
+const TOKEN = process.env.MAILTRAP_TOKEN || "4ecc8fa8698648d18620ee191de0d057"; 
 
 export const mailtrapClient = new MailtrapClient({
   token: TOKEN,
